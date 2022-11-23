e792551439abc5a355a150b32f649f6c0e6b0512
sina <sinaghorbanpour1378@gmail.com>
2022-11-23 18:00:53 +0330

    ready for deployment

---
+++ src/components/SearchBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+    this.props.doWhenSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Search Videos</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => {
+                this.setState({ term: e.target.value });
+              }}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
